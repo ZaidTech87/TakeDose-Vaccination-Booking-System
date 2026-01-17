c6c28036cb21d06ba2daa47c1dc37d7b7330422f
Mohd Zaid <zaidalig1234@gmail.com>
2026-01-17 13:51:49 +0530

    this is firt

---
+++ TakeDose-Vaccination-Booking-System/src/main/java/com/example/TakeDose_Vaccination/_Booking_System/TakeDoseVaccinationBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.TakeDose_Vaccination._Booking_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeDoseVaccinationBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakeDoseVaccinationBookingSystemApplication.class, args);
+	}
+
+}
---
+++ TakeDose-Vaccination-Booking-System/src/test/java/com/example/TakeDose_Vaccination/_Booking_System/TakeDoseVaccinationBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TakeDose_Vaccination._Booking_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakeDoseVaccinationBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
