946664f15acf0171f1c9b0979971f301ca85acef
Mohd Zaid <zaidalig1234@gmail.com>
2026-01-24 15:38:37 +0530

    send teenty four

--- TakeDose-Vaccination-Booking-System/src/main/java/com/example/TakeDose_Vaccination/_Booking_System/Models/Dose.java
+++ TakeDose-Vaccination-Booking-System/src/main/java/com/example/TakeDose_Vaccination/_Booking_System/Models/Dose.java
@@ -1,4 +1,29 @@
 package com.example.TakeDose_Vaccination._Booking_System.Models;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.util.Date;
+
+@Entity
+@Table(name="dose")
+@Data
 public class Dose {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true)
+    private String doseId;
+
+    @CreationTimestamp
+    private Date vaccinationDate;
+
+    @OneToOne
+    @JoinColumn
+    private User user;
+
 }
