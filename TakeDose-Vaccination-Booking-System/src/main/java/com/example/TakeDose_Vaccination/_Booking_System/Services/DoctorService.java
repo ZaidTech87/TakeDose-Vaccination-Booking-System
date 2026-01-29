edeb5dcc5bb64ecad7b05359dacc9e615899e66a
Mohd Zaid <zaidalig1234@gmail.com>
2026-01-29 19:38:41 +0530

    ors

--- TakeDose-Vaccination-Booking-System/src/main/java/com/example/TakeDose_Vaccination/_Booking_System/Services/DoctorService.java
+++ TakeDose-Vaccination-Booking-System/src/main/java/com/example/TakeDose_Vaccination/_Booking_System/Services/DoctorService.java
@@ -1,4 +1,16 @@
 package com.example.TakeDose_Vaccination._Booking_System.Services;
 
+import com.example.TakeDose_Vaccination._Booking_System.Models.Doctor;
+import com.example.TakeDose_Vaccination._Booking_System.Repository.DoctorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class DoctorService {
+    @Autowired
+    private DoctorRepository doctorRepository;
+
+    public String addDoctor(Doctor doctor){
+
+    }
 }
