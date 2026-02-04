5b27ba54e8d571079d8287caf73878ee3bc89688
Mohd Zaid <zaidalig1234@gmail.com>
2026-02-04 23:19:19 +0530

    2ne 4 jan

--- TakeDose-Vaccination-Booking-System/src/main/java/com/example/TakeDose_Vaccination/_Booking_System/Services/AppointmentService.java
+++ TakeDose-Vaccination-Booking-System/src/main/java/com/example/TakeDose_Vaccination/_Booking_System/Services/AppointmentService.java
@@ -3,8 +3,14 @@ package com.example.TakeDose_Vaccination._Booking_System.Services;
 import com.example.TakeDose_Vaccination._Booking_System.DTOs.AppointmentReqDto;
 import com.example.TakeDose_Vaccination._Booking_System.Exeptions.DoctorNotFound;
 import com.example.TakeDose_Vaccination._Booking_System.Exeptions.UserNotFound;
+import com.example.TakeDose_Vaccination._Booking_System.Models.Appointment;
+import com.example.TakeDose_Vaccination._Booking_System.Models.Doctor;
+import com.example.TakeDose_Vaccination._Booking_System.Models.User;
 import com.example.TakeDose_Vaccination._Booking_System.Repository.AppointmentRepository;
+import com.example.TakeDose_Vaccination._Booking_System.Repository.DoctorRepository;
+import com.example.TakeDose_Vaccination._Booking_System.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -12,9 +18,34 @@ import java.util.Optional;
 @Service
 public class AppointmentService {
     @Autowired
-    AppointmentRepository appointmentRepository;
+   private  AppointmentRepository appointmentRepository;
+    @Autowired
+   private DoctorRepository doctorRepository;
+    @Autowired
+   private UserRepository userRepository;
+    @Autowired
+    private JavaMailSender mailSender;
+
     public String bookAppointment(AppointmentReqDto appointmentReqDto)throws DoctorNotFound, UserNotFound {
-        Optional<>
+        Optional<Doctor> doctorOptional = doctorRepository.findById(appointmentReqDto.getDocId());
+        if(!doctorOptional.isPresent()){
+            throw new DoctorNotFound("Doctor Not Found");
+        }
+
+        Optional<User> userOptional = userRepository.findById(appointmentReqDto.getUserId());
+        if(!userOptional.isPresent()){
+            throw new UserNotFound("User Not Found");
+
+        }
+
+        Doctor doctor = doctorOptional.get();
+        User user = userOptional.get();
+
+        Appointment appointment = new  Appointment();
+        appointment.setAppointmentDate(appointmentReqDto.getAppointmentDate());
+        appointment.setAppointmetTime(appointmentReqDto.getAppointmentTime());
+
+
 
     }
 
