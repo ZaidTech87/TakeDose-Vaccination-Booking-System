e49ad56ddd92cdfbdb508eb3890cb7ae9724a195
Mohd Zaid <zaidalig1234@gmail.com>
2026-02-07 16:01:51 +0530

    hell

--- TakeDose-Vaccination-Booking-System/src/main/java/com/example/TakeDose_Vaccination/_Booking_System/Services/DoseService.java
+++ TakeDose-Vaccination-Booking-System/src/main/java/com/example/TakeDose_Vaccination/_Booking_System/Services/DoseService.java
@@ -1,7 +1,9 @@
 package com.example.TakeDose_Vaccination._Booking_System.Services;
 
 import com.example.TakeDose_Vaccination._Booking_System.Models.Dose;
+import com.example.TakeDose_Vaccination._Booking_System.Models.User;
 import com.example.TakeDose_Vaccination._Booking_System.Repository.DoseRepository;
+import com.example.TakeDose_Vaccination._Booking_System.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +12,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class DoseService {
     @Autowired
     DoseRepository doseRepository;
-    public String giveDose(){}
+    @Autowired
+    UserRepository userRepository;
+    public String giveDose(String doseId, Integer userId) {
+        User user = userRepository.findById(userId).get();
+        Dose dose = new Dose();
+
+        dose.setDoseId(doseId);
+        dose.setUser(user);
+        user.setDose(dose);
+        userRepository.save(user);
+        doseRepository.save(dose);
+        return "Dose given successfully";
+
+    }
 
 }
