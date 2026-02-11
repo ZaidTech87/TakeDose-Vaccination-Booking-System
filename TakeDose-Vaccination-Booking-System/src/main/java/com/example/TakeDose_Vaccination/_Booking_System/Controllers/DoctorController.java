79ad7fc798b2eed86a0b51cbb743a936e59e798d
Mohd Zaid <zaidalig1234@gmail.com>
2026-02-12 00:31:11 +0530

    12 feb

--- TakeDose-Vaccination-Booking-System/src/main/java/com/example/TakeDose_Vaccination/_Booking_System/Controllers/DoctorController.java
+++ TakeDose-Vaccination-Booking-System/src/main/java/com/example/TakeDose_Vaccination/_Booking_System/Controllers/DoctorController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/doctor")
+@CrossOrigin(origins = "http://localhost:5173")
 public class DoctorController {
     @Autowired
     DoctorService doctorService;
--- TakeDose-Vaccination-Booking-System/src/main/java/com/example/TakeDose_Vaccination/_Booking_System/Controllers/UserController.java
+++ TakeDose-Vaccination-Booking-System/src/main/java/com/example/TakeDose_Vaccination/_Booking_System/Controllers/UserController.java
@@ -10,6 +10,7 @@ import java.util.Date;
 
 @RestController
 @RequestMapping("/user")
+@CrossOrigin(origins = "http://localhost:5173")
 public class UserController {
     @Autowired
     UserService userService;
